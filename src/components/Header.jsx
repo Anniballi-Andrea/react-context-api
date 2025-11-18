199a2043c50caf17ed8fde5dfe194a5b3b080835
Anniballi-Andrea <andreanniballi@gmail.com>
2025-11-18 15:37:17 +0100

    add NavBar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,48 +1,20 @@
 
 import { useContext } from "react";
-import { NavLink } from "react-router-dom";
+
 import BudgetContext from "../context/BudgetContext"
+import NavBar from "./NavBar";
 
 
 export default function Header() {
 
-    const prova = useContext(BudgetContext)
+    const { budgetMode, setBudgetMode } = useContext(BudgetContext)
+
 
 
     return (
 
         <header>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="/">BoolStore</a>
-                    <button
-                        className="navbar-toggler"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item p-3">
-                                <NavLink className="nav-link" to="/"  >Home</NavLink>
-                            </li>
-                            <li className="nav-item p-3">
-                                <NavLink className="nav-link" to="/About"  >About Us</NavLink>
-                            </li>
-                            <li className="nav-item p-3">
-                                <NavLink className="nav-link" to="/products" >Products</NavLink>
-                            </li>
-
-
-                        </ul>
-                    </div>
-                    <button className="btn">budget Mode:  </button>
-                </div>
-            </nav>
+            <NavBar />
         </header>
     )
 }
